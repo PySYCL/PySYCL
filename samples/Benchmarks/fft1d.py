f8a90ac619ea8a0617a291c71594a30803236ba9
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2024-04-06 14:38:23 -0400

    added benchmarks in pysycl samples

---
+++ samples/Benchmarks/fft1d.py
@@ -0,0 +1,45 @@
+import sys
+import time
+import numpy as np
+import torch  # Import PyTorch
+
+sys.path.insert(1, '../../build/')
+import pysycl
+
+pysycl_device = pysycl.device.get_device(0, 0)
+torch_device  = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+np.random.seed(37)
+
+N = 2**25 + 1
+
+# numpy vector setup
+A_np = np.random.rand(N).astype(np.float64)
+
+# pysycl and pytorch vector setup
+A_pysycl = pysycl.vector(A_np, device=pysycl_device, dtype=pysycl.double)
+A_torch  = torch.tensor(A_np, device=torch_device, dtype=torch.float64)
+
+# pysycl timings
+start_time_pysycl = time.time()
+A_pysycl_fft = pysycl.fft1d(A_pysycl)
+end_time_pysycl = time.time()
+pysycl_time = end_time_pysycl - start_time_pysycl
+
+print(f"PySYCL timings: {pysycl_time} seconds")
+
+# numpy timings
+start_time_np = time.time()
+A_np_fft = np.fft.fft(A_np)
+end_time_np = time.time()
+np_time = end_time_np - start_time_np
+
+print(f"NumPy timings: {np_time} seconds")
+
+# pytorch timings
+start_time_torch = time.time()
+A_torch_fft = torch.fft.fft(A_torch)
+end_time_torch = time.time()
+torch_time = end_time_torch - start_time_torch
+
+print(f"PyTorch timings: {torch_time} seconds")
--- samples/Fast_Fourier_Transform/fft1d.py
+++ samples/Fast_Fourier_Transform/fft1d.py
@@ -1,35 +1,18 @@
 import sys
 import time
 import numpy as np
+import torch  # Import PyTorch
 
 sys.path.insert(1, '../../build/')
 import pysycl
 
 device = pysycl.device.get_device(0, 0)
-
 np.random.seed(37)
 
-N = 2**26 + 1
+N = 2**3
 
 A_np = np.random.rand(N).astype(np.float64)
-A_pysycl = pysycl.vector(A_np, device = device, dtype= pysycl.double)
+A_pysycl = pysycl.vector(A_np, device=device, dtype=pysycl.double)
 
-# pysycl timings
-start_time_pysycl = time.time()
 A_pysycl_fft = pysycl.fft1d(A_pysycl)
-end_time_pysycl = time.time()
-pysycl_time = end_time_pysycl - start_time_pysycl
-
-# numpy timings
-start_time_np = time.time()
 A_np_fft = np.fft.fft(A_np)
-end_time_np = time.time()
-np_time = end_time_np - start_time_np
-
-# print(A_np_fft)
-
-# for i in A_pysycl_fft:
-#   print(i)
-
-print(f"pysycl timings: {pysycl_time} seconds")
-print(f"numpy timings: {np_time} seconds")
